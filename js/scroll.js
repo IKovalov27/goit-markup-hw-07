d3a4eb777ed03645eaf2a8c3f6090e200bb73bbb
Illia Kovalov <kovalev.illia2009@gmail.com>
2024-01-08 23:23:16 +0100

    27, final

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,13 @@
 $(function () {
+    $("a.scrolltohead").click(function () {
+     let elementClick = $(this).attr("href")
+     let destination = $(elementClick).offset().top;
+     $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 750);
+     return false;
+    });
+});
+
+$(function () {
     $("a.scrollto").click(function () {
      let elementClick = $(this).attr("href")
      let destination = $(elementClick).offset().top;
