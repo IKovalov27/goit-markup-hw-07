c87c437de5e8328ff9380ab4288ac7c583f54fe1
Illia Kovalov <kovalev.illia2009@gmail.com>
2024-01-09 00:10:31 +0100

    171717

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,18 @@
-$(function () {
+/*$(function () {
     $("a.scrollto").click(function () {
      let elementClick = $(this).attr("href")
      let destination = $(elementClick).offset().top;
      $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 750);
      return false;
+    });
+});*/
+
+
+$(function () {
+    $("a.scrollto").click(function () {
+        let elementClick = $(this).attr("href");
+        let destination = $(elementClick).offset().top;
+        $('html, body').animate({ scrollTop: destination }, 750);
+        return false;
     });
 });
